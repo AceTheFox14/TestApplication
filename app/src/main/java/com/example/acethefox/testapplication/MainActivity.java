ea3d36bd06e730d3daf5635e9cda8e5581689424
AceTheFox14 <acethefox14@gmail.com>
2018-03-19 17:05:40 -0400

    Initial commit

---
+++ app/src/main/java/com/example/acethefox/testapplication/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.acethefox.testapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.appbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        TextView hello = (TextView) findViewById(R.id.hello_label);
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                hello.setText("Settings clicked but there are no settings");
+                return true;
+
+            case R.id.item2_menu:
+                hello.setText("Item 2 Clicked!");
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+}
+
